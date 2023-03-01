42db959ed264c7b4a757447cee0f22d45d1cf47c
weatherpub <patrick@bayareamedia.net>
2023-02-28 16:43:57 -0800

    Personal Class

---
+++ 3-Personal/src/main/java/edu/sfsu/Personal.java
@@ -0,0 +1,47 @@
+package edu.sfsu;
+
+public class Personal {
+    private String name;
+    private String address;
+    private int age;
+    private int phone;
+
+    public Personal(String name, String address, int age, int phone) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
