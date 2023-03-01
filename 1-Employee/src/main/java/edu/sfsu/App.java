6b6ad006b90fd2ca48de59d55ea8da810e511333
weatherpub <patrick@bayareamedia.net>
2023-02-28 16:25:03 -0800

    Newer App.java

--- 1-Employee/src/main/java/edu/sfsu/App.java
+++ 1-Employee/src/main/java/edu/sfsu/App.java
@@ -1,13 +1,54 @@
 package edu.sfsu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+<<<<<<< HEAD
+public class App {
+    public static void main( String[] args) {
+        int i = 0;
+        System.out.println( "Car Class" );
+        Car car = new Car("Mustang", 1968);
+        System.out.println("[Make] " + car.getMake());
+        System.out.println("[Year] " + car.getYear());
+
+        do {
+            car.accelerate();
+            System.out.println("Accelerating Current Speed: " + car.getSpeed());
+            i++;
+        } while(i < 4);
+
+        do {
+            car.brake();
+            System.out.println("Braking Current Speed: " + car.getSpeed());
+            i--;
+        } while(i > 0);
     }
 }
+=======
+import java.util.stream.DoubleStream;
+public class App {
+    static double subtract(double... operands) {
+       return DoubleStream.of(operands).sum();
+    }
+    static double add(double... operands) {
+        return DoubleStream.of(operands).sum();
+    }
+    public static void main( String[] args ) {
+        Employee obj1 = new Employee(0,"Accounting","Susan Meyers", "Vice President");
+        Employee obj2 = new Employee(1,"Accounting", "Vice President");
+
+        int id = obj1.getIdNumber();
+        String name = obj1.getName();
+        String department = obj1.getDepartment();
+        String position = obj1.getDepartment();
+
+        System.out.println(name + " " + id + " " + department + " " + position);
+
+        id = obj2.getIdNumber();
+        department = obj2.getDepartment();
+        position = obj2.getPosition();
+
+        System.out.println(id + " " + department + " " + position);
+
+//        Employee em02 = new Employee(1, "IT", "Programmer");
+    }
+}
+>>>>>>> 8620326 (initial add)
